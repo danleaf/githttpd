43e27f52fa659e098ece35ac48dfec1cd3c7457a
danleaf <danleaf@foxmail.com>
2017-09-09 16:36:41 +0800

    123

--- autorun/Service1.cs
+++ autorun/Service1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
@@ -11,6 +12,8 @@ namespace autorun
 {
     public partial class Service1 : ServiceBase
     {
+        Process pr;
+
         public Service1()
         {
             InitializeComponent();
@@ -18,15 +21,41 @@ namespace autorun
 
         protected override void OnStart(string[] args)
         {
+            StreamReader sr = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "command");
+            var cmd = sr.ReadLine().Trim();
+            sr.Close();
+
+            int space = -1;
+            space = cmd.IndexOf(' ');
+            if (space < 0)
+                space = cmd.IndexOf('\t');
+
+
             ProcessStartInfo info = new ProcessStartInfo();
-            info.FileName = AppDomain.CurrentDomain.BaseDirectory + "cmd.bat";
+            info.FileName = space < 0 ? cmd : cmd.Substring(0, space);
+            info.Arguments = cmd.Substring(space).Trim();
             info.WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            info.WindowStyle = ProcessWindowStyle.Minimized;
-            Process.Start(info);
+            info.CreateNoWindow = true;
+            pr = Process.Start(info);
         }
 
         protected override void OnStop()
         {
+            try
+            {
+                pr.Kill();
+            }
+            catch (Exception e)
+            {
+                try
+                {
+                    StreamWriter sr = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "autorun-error.log");
+                    sr.WriteLine("stop command process failed");
+                    sr.Write(e.Message);
+                    sr.Close();
+                }
+                catch (Exception) { }
+            }
         }
     }
 }
