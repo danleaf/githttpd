a74f2ab5a946f02d4dcbefc839b5b51661a0d747
danleaf <danleaf@foxmail.com>
2017-09-09 15:36:06 +0800

    4

---
+++ autorun/Service1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace autorun
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = AppDomain.CurrentDomain.BaseDirectory + "cmd.bat";
+            info.WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            info.WindowStyle = ProcessWindowStyle.Minimized;
+            Process.Start(info);
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
